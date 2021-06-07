459c8b88a74410f4ff9443e28beae86ab614d5fd
Eugen1y <evgeniykolodko@gmail.com>
2021-06-07 23:02:28 +0300

    update

--- Project/urls.py
+++ Project/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cart/', include(('cart.urls','cart'), namespace='cart')),
+    path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
     path('', include('product.urls')),
     path('', include('user.urls')),
     path('', include('view.urls'))
